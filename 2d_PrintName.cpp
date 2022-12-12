23a2dafc862e7ff084a84b903f2c80f118a60ea7
Rupal48 <120312408+Rupal48@users.noreply.github.com>
2022-12-12 11:56:16 +0530

    Add files via upload

---
+++ 2d_PrintName.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+class myclass
+{
+    public:
+    void displaydata()
+    {
+        cout << "My name is Rupal malhotra." << endl;
+    }
+};
+int main()
+{
+    myclass a;
+    a.displaydata();
+    return 0;
+}
